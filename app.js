a4ef2024947c73a31c6fe6933d1a0c599cc1c441
Phila <85244615+PhilaNgcamu@users.noreply.github.com>
2022-03-28 00:50:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const navMotion = () => {
+  const hamburger = document.querySelector(".hamburger-menu"),
+    navigation = document.querySelector(".nav-links"),
+    navLinks = document.querySelectorAll(".nav-links li");
+
+  hamburger.addEventListener("click", () => {
+    navigation.classList.toggle("nav-motion");
+
+    navLinks.forEach((eachLink, eachIndex) => {
+      if (eachLink.style.animation) eachLink.style.animation = "";
+      else
+        eachLink.style.animation = `navFade 0.5s ease-in forwards ${
+          eachIndex / 9 + 0.2
+        }s`;
+    });
+    hamburger.classList.toggle("toggle");
+  });
+};
+
+navMotion();
